d2b64f5c3a838c45cccdc2724f2434b334d68ace
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-11 09:11:30 -1100

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''''This module provides function to return concatinated string.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
