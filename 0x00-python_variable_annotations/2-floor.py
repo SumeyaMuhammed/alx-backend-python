21206fbe59cc9d917e8910eb5b044569b6948d02
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-11 09:13:54 -1100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''This module provides function to return the floor of a float.
+'''
+
+
+import math
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return math.floor(a)
