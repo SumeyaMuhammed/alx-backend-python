73908a9198649e49fabc94530e4d0d08984127f4
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-11 09:07:23 -1100

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This module provides function to return the sum of two floats
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''returns the sum of  two floating-point numbers.
+    '''
+    return a + b
