f49ab4110a7bc9777365398ccc6175b095ce4fde
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-11 09:15:46 -1100

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This module provides function to return the string representation of a float.
+'''
+
+
+def to_str(n: float) -> str:
+    '''Cast a floting type number in to a string.
+    '''
+    return str(n)
